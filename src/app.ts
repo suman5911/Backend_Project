ce89b1914574f30d54d043b58622eb01850bad3f
Sumanpreet SIngh <ssingh28@rrc.ca>
2026-04-04 15:54:20 -0400

    add morgan logging to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Express } from "express";
+import morgan from "morgan";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 
 // Initialize Express application
 const app: Express = express();
+
+// Use Morgan for HTTP request logging
+app.use(morgan("combined"));
 
 // Middleware to parse JSON
 app.use(express.json());
