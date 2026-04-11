7f60dc7e68b7507a0205feeedec1127e93cca3c8
Sumanpreet SIngh <ssingh28@rrc.ca>
2026-04-11 15:09:28 -0400

    protect routes with authentication

--- src/api/v1/routes/reviewRoutes.ts
+++ src/api/v1/routes/reviewRoutes.ts
@@ -1,6 +1,8 @@
 import express, { Router } from "express";
 import { validateRequest } from "../middleware/validate";
 import { reviewSchemas } from "../validation/reviewSchemas";
+import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize";
 import {
   getAllReviews,
   getReviewById,
@@ -13,8 +15,8 @@ const router: Router = express.Router();
 
 router.get("/reviews", getAllReviews);
 router.get("/reviews/:id", validateRequest(reviewSchemas.getById), getReviewById);
-router.post("/reviews", validateRequest(reviewSchemas.create), createReview);
-router.put("/reviews/:id", validateRequest(reviewSchemas.update), updateReview);
-router.delete("/reviews/:id", validateRequest(reviewSchemas.delete), deleteReview);
+router.post("/reviews", authenticate, isAuthorized({ hasRole: ["admin", "manager", "user"] }), validateRequest(reviewSchemas.create), createReview);
+router.put("/reviews/:id", authenticate, isAuthorized({ hasRole: ["admin", "manager", "user"] }), validateRequest(reviewSchemas.update), updateReview);
+router.delete("/reviews/:id", authenticate, isAuthorized({ hasRole: ["admin"] }), validateRequest(reviewSchemas.delete), deleteReview);
 
 export default router;
